7f5551f62841028c30b91e3b93bd1285f096cfa6
Ferin @ lapHP <ferinsharaf23@gmail.com>
2025-11-05 20:48:23 +0530

    Fix app listen

--- app.js
+++ app.js
@@ -22,4 +22,6 @@ app.use('/', (req, res) => {
 })
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});

2116735474c6a793519c1b12db367b0225f74d5f
Ferin @ lapHP <ferinsharaf23@gmail.com>
2025-11-30 02:36:01 +0530

    Create updateProfile route

--- route/account.js
+++ route/account.js
@@ -7,6 +7,72 @@ const verifyToken = require('../controller/accountController.js').verifyToken
 
 const router = express.Router();
 
+router.post('/updateProfile', verifyToken, async (req, res) => {
+    console.log("\n\n\n\n\n\n\n\n\nreq recieved");
+    // res.status(401).json({message: "test"})
+    try {
+        console.log("Incoming user: ", req.user);
+        const userId = req.user.id;
+        console.log("Req body: ", req.body);
+        const {username, email} = req.body;
+
+        if(!username || !email) {
+            console.log("No username or email");
+            return res.status(400).json({message: "Missing Fields"});
+        }
+
+        const query = `Update users SET username = ?, email = ? WHERE id = ?`
+        console.log(query)
+        console.log();
+        console.log("Starting db query");
+        db.getConnection((err) => {
+            console.log("DB CONNECT ERROR:", err);
+        });
+        console.log("DB CONNECT SUCCESS");
+        // await db.query(query, [username, email, userId], (err, result) => {
+            //     if (err) {
+                //         console.error(err);
+                //         return res.status(500).json({message: "Database error"});
+                //     }
+
+                //     if (result.affectedRows === 0) {
+                    //         return res.status(404).json({message: "User not found"});
+        //     }
+
+        //     res.status(200).json({
+            //         message: "Profile Updates successfully",
+        //         updated: {username, email}
+        //     })
+        // })
+
+        try {
+            console.log("Entering db query");
+            const [result] = await db.query(query, [username, email, userId]);
+
+            if(result.affectedRows === 0) {
+                return res.status(404).json({message: "User not found"});
+            }
+            console.log(result)
+            return res.status(200).json({
+                message: "User Updated Successfully",
+                updated: { username, email }
+            })
+        } catch (err) {
+            console.error(err);
+            const msg = err.sqlMessage; 
+            const match = msg.match(/for key '(.+?)'/);
+            if(err.errno === 1062) {
+                const duplicateKey = match[1].split()[1]
+                return res.status(500).json({ message: `${duplicateKey} already exists` });
+                
+            }
+            return res.status(500).json({ message: "Database error" });
+        }
+    } catch (err) {
+        console.log(err);
+    }
+})
+
 router.post('/signup', async (req, res) => {
     const { username, password, email } = req.body;
 
@@ -41,14 +107,19 @@ router.post('/login', async (req, res) => {
         const valid = await bcrypt.compare(password, user.password);
         if(!valid)
             return res.status(400).json({message: "Invalid username or password"});
-        const token = jwt.sign({id:user.id, username: user.username}, process.env.JWT_SECRET_KEY);
+        const token = jwt.sign(
+            {id:user.id, username: user.username},
+            process.env.JWT_SECRET_KEY, 
+            { expiresIn: '1d'}
+        );
         resUser = {username: user.username, email: user.email, id: user.id};
+        console.log("Done")
         res.json({message: "Login Successful", token, user: resUser});
     } catch (err) {
         console.log(err);
         res.status(500).json({message: "Server Error. Cannot login"});
     }
-
+    
 })
 
 router.get('/profile', verifyToken, async (req, res) => {
